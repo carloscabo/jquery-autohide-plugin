f93385afe5a669705ea51471ce8a8b36162ec0b6
carloscabo <carloscabo@the-cocktail.com>
2014-04-16 12:25:31 +0200

    initial release

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-jquery-autohide-plugin
+JQuery Autohide plugin
 ======================
 
-Jquery plugin to show / hide / autohide elements, like shopping carts or help bubbles
+Jquery plugin to show / hide / autohide elements, like shopping carts or help bubbles. These elements visibility are triggered from anothe element... usually a button or so.
+
+![Smaple snapshop](https://raw.github.com/carloscabo/jquery-autohide-plugin/master/sample-image.png)
+
+
+## Usage
+
+    $('.element.to-hide-show').autohide_timeout({
+      buttons: '.button-1',
+      buttons_events: 'click', // default is click
+      timeout: 3000
+    });
+
+`.hide-show-element` the element / container we want to show / hide.
+
+`timeout` time until the element autohides in miliseconds.
+
+`buttons_events` events used for the button element. Default is `click` event, but you can also use `mouseenter` or both (see sample below).
+
+`.button` the element that acts as trigger of the visibility... usually a button. You can provide several elements at once with JQuery syntax (comma separated values) this way:
+
+    $('.element.to-hide-show').autohide_timeout({
+      buttons: '.button-1, .button-2, .button-3',
+      buttons_events: 'click mouseenter',
+      timeout: 3000
+    });
---
+++ jquery.autohide.js
@@ -0,0 +1,63 @@
+/*
+JQuery Autohide by Carlos Cabo
+https://github.com/carloscabo/jquery-autohide-plugin
+
+// Usage
+$('.element.to-hide-show').autohide_timeout({
+  buttons: '.button-1',
+  buttons_events: 'click', // default is click
+  timeout: 3000
+});
+*/
+
+(function($) {
+
+$.fn.autohide_timeout = function(options) {
+
+  return this.each(function() {
+    new aht_init(this, options);
+  });
+
+  function aht_init(el, options){
+    var
+      $el  = $(el),
+      $btn = $(options['buttons']),
+      btn_events = 'click',
+      ms  = options['timeout'];
+
+    $el.ah_timeout = null;
+    if (options['buttons_events'] !== undefined) {
+      btn_events = options['buttons_events'];
+    }
+
+    $el.hide();
+
+    $btn.on(btn_events, function(e) {
+      e.preventDefault();
+      $el.fadeToggle('fast', 'linear');
+      clearTimeout($el.ah_timeout);
+      $el.ah_timeout = setTimeout(function(){
+        $el.aht_hide();
+      }, ms);
+    });
+
+    $el.on('mouseenter', function(e) {
+      e.preventDefault();
+      clearTimeout($el.ah_timeout);
+    });
+
+    $el.on('mouseleave', function(e) {
+      e.preventDefault();
+      $el.ah_timeout = setTimeout(function(){
+        $el.aht_hide();
+      }, 400);
+    });
+  }
+};
+
+$.fn.aht_hide = function(el) {
+  $(this).fadeOut('fast');
+  clearTimeout($(this).ah_timeout);
+};
+
+}(jQuery));
